0e9235c267c9b7ca1a4e464d15dc4111d0cf9968
Erika Santana <erika.araujo12344@gmail.com>
2021-07-08 22:30:11 -0300

    em andamento

---
+++ telaPrincipal/app.js
@@ -0,0 +1,17 @@
+function adicionarFilme() {
+    const campoFilme = document.querySelector('#filme')
+    const filmeFavorito = campoFilme.value
+    
+    if(filmeFavorito.endsWith('.jpg')) {
+        listarFilmeNaTela(filmeFavorito)
+    } else {
+        alert('Imagem inválida')
+    }
+    campoFilme.value = '' //limpar campo
+}
+
+function listarFilmeNaTela(filme) {
+    const listarFilmes = document.querySelector('#listaFilmes')
+    const elementoFilme = "<img src=" + filme + ">"
+    listaFilmes.innerHTML = listarFilmes.innerHTML + elementoFilme
+}
