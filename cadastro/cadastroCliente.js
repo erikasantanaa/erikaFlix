ffe6dbafecb329c4ea9a4523d81fdccafc116d97
Erika Santana <erika.araujo12344@gmail.com>
2021-10-23 23:43:25 -0300

    em andamento

--- cadastro/cadastroCliente.js
+++ cadastro/cadastroCliente.js
@@ -1,3 +1,5 @@
+
+//Pega valores do cadastro em geral
 function pegaValores() {
     let usuario = document.getElementById('usuario').value
     let email = document.getElementById('email').value
@@ -14,15 +16,19 @@ function pegaValores() {
     this.validaCampos(objeto)
 };
 
+/*Campos Preenchidos: 
+(Senhas iguais validar), 
+Senhas diferentes (gerar alerta), 
+Falta Caracteres (gerar alerta),
 
+*/
  function validaCampos(objeto) {
     let camposPreenchidos = objeto.usuario.length > 0 && objeto.email.length > 0 
     && objeto.senha.length > 0 && objeto.repetirSenha.length > 0
-
     console.log(camposPreenchidos)
 
-    if(camposPreenchidos) {
-            console.log('Validação de campos Ok')
+   if(camposPreenchidos) {
+            console.log('Validação de campos Ok') //
             if(objeto.senha === objeto.repetirSenha) {
                 console.log('Senhas iguais')
             } else {
@@ -32,3 +38,13 @@ function pegaValores() {
         console.log('Validação de campos Negado')
     };
  };
+
+ let senha = document.getElementById('senha').value
+if (senha.length > 7){
+   console.log('teste')
+}
+
+ //Comprimento de caracteres e numeros nos campos
+
+
+ 
